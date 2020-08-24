e00bf09286d69b0cecbbcc6603b1d2c059a6db01
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-08-24 15:48:08 +0300

    commit 24.08.2020

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <>
-      <h1>Hello, World!!!</h1>
-      <Button variant="success">Success</Button>
-    </>
+    <div>
+      <Header />
+    </div>
   );
 }
 
