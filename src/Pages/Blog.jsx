398d6a8d328cb7ec23bee9f2676190e7c3c0a784
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-08-25 00:18:42 +0300

    commit 1 25.08.2020

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,11 +1,90 @@
 import React, { Component } from 'react'
+import { Container, Row, Col, Media, Card, ListGroup } from 'react-bootstrap';
+import pic_logo from '../assets/logo.svg'
+
 
 export default class Blog extends Component {
     render() {
         return (
-            <div>
-                Blog
-            </div>
+            <Container>
+                <Row>
+                    <Col md='9'>
+                        <Media border="success" className="mt-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src={pic_logo}
+                                alt="pic"
+                            />
+                            <Media.Body>
+                                <h5>Title of the post</h5>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias nemo laboriosam, totam soluta adipisci maxime dignissimos cum velit suscipit temporibus molestiae quis sed quibusdam sunt accusantium, exercitationem ipsum eaque. Nostrum?</p>
+                            </Media.Body>
+                        </Media>
+
+                        <Media border="success" className="mt-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src={pic_logo}
+                                alt="pic"
+                            />
+                            <Media.Body>
+                                <h5>Title of the post</h5>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias nemo laboriosam, totam soluta adipisci maxime dignissimos cum velit suscipit temporibus molestiae quis sed quibusdam sunt accusantium, exercitationem ipsum eaque. Nostrum?</p>
+                            </Media.Body>
+                        </Media>
+
+                        <Media border="success" className="mt-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src={pic_logo}
+                                alt="pic"
+                            />
+                            <Media.Body>
+                                <h5>Title of the post</h5>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias nemo laboriosam, totam soluta adipisci maxime dignissimos cum velit suscipit temporibus molestiae quis sed quibusdam sunt accusantium, exercitationem ipsum eaque. Nostrum?</p>
+                            </Media.Body>
+                        </Media>
+
+                        <Media border="success" className="mt-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src={pic_logo}
+                                alt="pic"
+                            />
+                            <Media.Body>
+                                <h5>Title of the post</h5>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias nemo laboriosam, totam soluta adipisci maxime dignissimos cum velit suscipit temporibus molestiae quis sed quibusdam sunt accusantium, exercitationem ipsum eaque. Nostrum?</p>
+                            </Media.Body>
+                        </Media>
+                    </Col>
+                    <Col md="3">
+                        <h5 className="text-center mt-5">Categories</h5>
+                        <Card>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>Html / CSS</ListGroup.Item>
+                                <ListGroup.Item>JavaScript</ListGroup.Item>
+                                <ListGroup.Item>Erlang</ListGroup.Item>
+                                <ListGroup.Item>React</ListGroup.Item>
+                                <ListGroup.Item>Assembler</ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                        <Card bg="light" className="mt-5">
+                            <Card.Body>
+                                <Card.Title>Side some whatever</Card.Title>
+                                <Card.Text>Some text</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
 import CarouselBox from '../Components/CarouselBox';
+import { Container, Button, CardDeck, Card } from 'react-bootstrap';
+import tea_1 from '../assets/tea_1.jpg';
+import tea_2 from '../assets/tea_2.jpg';
+import tea_3 from '../assets/tea_3.jpg';
 
 export default class Home extends Component {
     render() {
         return (
-            <CarouselBox />
+            <>
+                <CarouselBox />
+
+                <Container>
+                    <h2 className="text-center m-4">Our team</h2>
+                    <CardDeck className="m-4">
+                        <Card border="success" text="primary">
+                            <Card.Img variant="top" src={tea_1} />
+                            <Card.Body>
+                                <Card.Title>The title 1</Card.Title>
+                                <Card.Text>Text</Card.Text>
+                                <Button variant="primary">About Picture 1</Button>
+                            </Card.Body>
+                        </Card>
+
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>The title 2</Card.Title>
+                                <Card.Text>Text</Card.Text>
+                                <Button variant="primary">About Picture 2</Button>
+                            </Card.Body>
+                            <Card.Img variant="bottom" src={tea_2} />
+
+                        </Card>
+
+                        <Card>
+                            <Card.Img variant="top" src={tea_3} />
+                            <Card.Body>
+                                <Card.Title>The title 3</Card.Title>
+                                <Card.Text>Text</Card.Text>
+                                <Button variant="primary">About Picture 3</Button>
+                            </Card.Body>
+                        </Card>
+                    </CardDeck>
+
+                </Container>
+            </>
         )
     }
 }
